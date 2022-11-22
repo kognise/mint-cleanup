258929db9251678083be3b6a5e03afa196a38936
Kognise <felix.mattick@gmail.com>
2022-11-22 08:37:35 -0600

    Add Black Cap

--- categorizer.js
+++ categorizer.js
@@ -3,6 +3,7 @@ module.exports.categoryMatchers = [
 	[ /VILLAGE WINE AND COFFE SHELBURNE VT/, 'Coffee Shops' ],
 	[ /LAKE CHAMPLAIN CHOCOLA BURLINGTON VT/, 'Coffee Shops' ],
 	[ /NOMAD COFFEE Burlington VT/, 'Coffee Shops' ],
+	[ /BC BURLINGTON CHURCH S BURLINGTON VT/, 'Coffee Shops' ],
 	[ /ACTRANSIT [A-Z0-9]+ SAN FRANCISCO CA/, 'Public Transportation' ],
 	[ /HOMEPORT BURLINGTON VT/, 'Furnishings' ],
 	[ /ExxonMobil/, 'Fast Food' ]
